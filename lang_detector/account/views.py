7b9a0dfb1257574f187440deec4a10c3c7fbd3fc
Sayyed <sayyed@fuelup.co>
2019-05-21 11:30:13 +0200

    user account management added

---
+++ lang_detector/account/views.py
@@ -0,0 +1,101 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.views import View
+
+from .models import Profile
+from .forms import UserRegistrationForm, ProfileEditForm, UserEditForm
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+        context = {
+            'user_form': user_form,
+            'profile_form': profile_form}
+    return render(request, 'account/edit.html', context)
+
+
+# def register(request):
+#     if request.method == 'POST':
+#         user_form = UserRegistrationForm(request.POST)
+#         if user_form.is_valid():
+#             # Create a new user object but avoid saving it yet
+#             new_user = user_form.save(commit=False)
+#             # Set the chosen password
+#             new_user.set_password(
+#                 user_form.cleaned_data['password']
+#             )
+#             # Save the User object
+#             new_user.save()
+#             Profile.objects.create(user=new_user)
+#             return render(request, 'account/register_done.html', {'new_user': new_user})
+#     else:
+#         user_form = UserRegistrationForm()
+#     return render(request, 'account/register.html', {'user_form': user_form})
+
+
+class Register(View):
+    """Class for handling user registretion."""
+
+    def get(self, request, *args, **kwargs):
+        user_form = UserRegistrationForm()
+        context = {'user_form': user_form}
+        return render(request, 'account/register.html', context)
+
+    def post(self, request, *args, **kwargs):
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+            # Save the User object
+            new_user.save()
+            Profile.objects.create(user=new_user)
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
